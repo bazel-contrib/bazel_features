42dcc8956b9ce97ab7c18d708c3a3a6471f1738f
Alex Eagle <alex@aspect.dev>
2025-10-15 10:50:33 -0700

    Update release_prep.sh

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -11,10 +11,13 @@ ARCHIVE="bazel_features-$TAG.tar.gz"
 git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
 
-# Sadly --attempt_to_print_relative_paths does not make these execroot-relative
-bazel cquery --output=files 'kind(starlark_doc_extract, //...)' \
-  | sed 's|^bazel-out/[^/]*/bin/||' \
-  | tar --create --auto-compress --directory "$(bazel info bazel-bin)" --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" --files-from=-
+# Add generated API docs to the release, see https://github.com/bazelbuild/bazel-central-registry/issues/5593
+docs="$(mktemp -d)"; targets="$(mktemp)"
+bazel --output_base="$docs" query --output=label --output_file="$targets" 'kind("starlark_doc_extract rule", //...)'
+bazel --output_base="$docs" build --target_pattern_file="$targets"
+tar --create --auto-compress \
+    --directory "$(bazel --output_base="$docs" info bazel-bin)" \
+    --file "$GITHUB_WORKSPACE/${ARCHIVE%.tar.gz}.docs.tar.gz" .
 
 cat << EOF
 ## Using Bzlmod with Bazel 6
